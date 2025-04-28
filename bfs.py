c4f72fccc064fe2ced18e77e352b1fd9380087f6
RaoSinchan <166647674+RaoSinchan@users.noreply.github.com>
2025-04-28 23:21:58 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -29,3 +29,27 @@ start_node = input("Enter the starting node for BFS: ")
 
 print("\nBFS Traversal:")
 bfs(visited, graph, start_node)
+
+
+
+
+'''
+#Enter the number of nodes: 6
+Enter node name: A
+Enter the neighbors of A separated by space: B C
+Enter node name: B
+Enter the neighbors of B separated by space: D E
+Enter node name: C
+Enter the neighbors of C separated by space: F
+Enter node name: D
+Enter the neighbors of D separated by space: 
+Enter node name: E
+Enter the neighbors of E separated by space: F
+Enter node name: F
+Enter the neighbors of F separated by space:
+
+Enter the starting node for BFS: A
+
+BFS Traversal:
+A B C D E F
+'''
